a45bc7a5b2fefc3017344cd0164ce509cc0222a3
lj200612 <colab061215@gmail.com>
2024-12-13 18:58:38 +0800

    T

--- 1127.c
+++ 1127.c
@@ -1,42 +1,41 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int m, p, n;
-    scanf("%d %d %d", &m, &p, &n);  
+    int m, n, p;
+    scanf("%d %d %d", &m, &p, &n);
 
-    int A[m][p], B[p][n], C[m][n];  
 
-    
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < p; j++) {
-            scanf("%d", &A[i][j]);
+    int a[m][p];
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < p; j++) {
+            scanf("%d", &a[i][j]);
         }
     }
 
-    
-    for (int i = 0; i < p; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &B[i][j]);
+
+    int b[p][n];
+    for(int i = 0; i < p; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &b[i][j]);
         }
     }
 
-    
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            C[i][j] = 0;  
+    int c[m][n];
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            c[i][j] = 0;
             for (int k = 0; k < p; k++) {
-                C[i][j] += A[i][k] * B[k][j]; 
+                c[i][j] += a[i][k] * b[k][j];
             }
         }
     }
-
     
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            printf("%d ", C[i][j]);
+            printf("%d ", c[i][j]);
         }
         printf("\n");
     }
-
+    printf("\n");
     return 0;
 }
--- 1130.c
+++ 1130.c
@@ -1,31 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
     int n;
-    scanf("%d", &n);  
+    scanf("%d", &n);
 
+    int y[n][n];
 
-    int yanghui[n][n];
+    for(int i = 0; i < n; i++) {
+        y[i][0] = 1;
+        y[i][i] = 1;
 
-    
-    for (int i = 0; i < n; i++) {
-       
-        yanghui[i][0] = 1;
-        yanghui[i][i] = 1;
-        
-        
-        for (int j = 1; j < i; j++) {
-            yanghui[i][j] = yanghui[i - 1][j - 1] + yanghui[i - 1][j];
+        for(int j = 1; j < i; j++) {
+            y[i][j] = y[i - 1][j - 1] +y[i - 1][j];
         }
     }
 
-    
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         for (int j = 0; j <= i; j++) {
-            printf("%d ", yanghui[i][j]);
+            printf("%d ", y[i][j]);
         }
         printf("\n");
     }
-
     return 0;
 }
